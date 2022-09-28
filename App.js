f0ae37c60a8c36ac374d094da3483035037b900d
chinfanster <chinfanster@gmail.com>
2022-09-27 23:17:04 -0700

    finsih viewimagescreen

--- App.js
+++ App.js
@@ -19,7 +19,7 @@ import Card from "./app/components/Card";
 import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
 
 export default function App() {
-  return <ListingDetailsScreen></ListingDetailsScreen>;
+  return <ViewImageScreen></ViewImageScreen>;
 }
 
 //
