6b8c230590b386f7955b563b438f8dcba8a65234
chinfanster <chinfanster@gmail.com>
2022-10-03 11:02:37 -0700

    minor updates

--- App.js
+++ App.js
@@ -1,14 +1,58 @@
-import React, { useState, useEffect } from "react";
-import * as ImagePicker from "expo-image-picker";
-import * as Permissions from "expo-permissions";
+import * as React from "react";
+import { View, Text, Button } from "react-native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NavigationContainer, useNavigation } from "@react-navigation/native";
 
 import Screen from "./app/components/Screen";
-import { Button, Image } from "react-native";
-import ImageInput from "./app/components/ImageInput";
-import ImageInputList from "./app/components/ImageInputList";
-import LoginScreen from "./app/screens/LoginScreen";
-import ListingEditScreen from "./app/screens/ListingEditScreen";
+
+function Link() {
+  const navigation = useNavigation();
+  return (
+    <Button
+      title="Click"
+      onPress={() => navigation.navigate("TweetDetails")}
+    ></Button>
+  );
+}
+
+function Tweets({ navigation }) {
+  return (
+    <Screen>
+      <Text>Tweets</Text>
+      <Button
+        title="View Tweet"
+        onPress={() => navigation.navigate("TweetDetails", { id: "1" })}
+      ></Button>
+    </Screen>
+  );
+}
+
+function TweetDetails({ route }) {
+  return (
+    <Screen>
+      <Text>Tweet Details {route.params.id}</Text>
+    </Screen>
+  );
+}
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-  return <ListingEditScreen></ListingEditScreen>;
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerStyle: { backgroundColor: "dodgerblue" },
+          headerTintColor: "white",
+        }}
+      >
+        <Stack.Screen name="Tweets" component={Tweets}></Stack.Screen>
+        <Stack.Screen
+          name="TweetDetails"
+          component={TweetDetails}
+          options={{ headerStyle: { backgroundColor: "tomato" } }}
+        ></Stack.Screen>
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
