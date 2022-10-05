37fc4d4f1a37a912f18ddbf30479795b01d5704f
chinfanster <chinfanster@gmail.com>
2022-10-05 11:03:09 -0700

    finish splash screen

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { NavigationContainer, useNavigation } from "@react-navigation/native";
 import "expo-dev-menu";
 import jwtDecode from "jwt-decode";
+import * as SplashScreen from "expo-splash-screen";
 
 import navigationTheme from "./app/navigation/navigationTheme";
 import AppNavigator from "./app/navigation/AppNavigator";
@@ -12,20 +13,43 @@ import authStorage from "./app/auth/storage";
 
 export default function App() {
   const [user, setUser] = useState();
+  const [isReady, setIsReady] = useState(false);
 
   const restoreToken = async () => {
     const token = await authStorage.getToken();
     if (!token) return;
     setUser(jwtDecode(token));
   };
+
+  const prepare = async () => {
+    try {
+      await SplashScreen.preventAutoHideAsync();
+      await restoreToken();
+    } catch (error) {
+      console.log("Error loading app", error);
+    } finally {
+      // Tell the application to render
+      setIsReady(true);
+    }
+  };
+
   useEffect(() => {
-    restoreToken();
+    prepare();
   }, []);
+
+  const onNavigationContainerReady = useCallback(async () => {
+    if (isReady) await SplashScreen.hideAsync();
+  }, [isReady]);
+
+  if (!isReady) return null;
 
   return (
     <AuthContext.Provider value={{ user, setUser }}>
       <OfflineNotice></OfflineNotice>
-      <NavigationContainer theme={navigationTheme}>
+      <NavigationContainer
+        theme={navigationTheme}
+        onReady={onNavigationContainerReady}
+      >
         {user ? <AppNavigator></AppNavigator> : <AuthNavigator></AuthNavigator>}
       </NavigationContainer>
     </AuthContext.Provider>
