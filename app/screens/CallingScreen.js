114f4352870628fcbdaeff3d734006d6025d34f6
chinfanster <chinfanster@gmail.com>
2023-04-17 13:22:19 -0700

    add more helper functions into CallingScreen.

--- app/screens/CallingScreen.js
+++ app/screens/CallingScreen.js
@@ -25,6 +25,7 @@ import {
   doc,
 } from "firebase/firestore";
 import { auth, db } from "../../firebase";
+import VideoScreen from "./VideoScreen";
 
 const peerConstraints = {
   iceServers: [
@@ -37,6 +38,7 @@ const peerConstraints = {
 export default function CallingScreen() {
   const [localStream, setLocalStream] = useState(null);
   const [remoteStream, setRemoteStream] = useState(null);
+  const [gettingCall, setGettingCall] = useState(false);
 
   const connecting = useRef(false);
   let pc = useRef(false);
@@ -111,6 +113,22 @@ export default function CallingScreen() {
     }
   }
 
+  /**
+   * For disconnectign the call, close the connection, release the stream,
+   * and delete the document for the call
+   **/
+
+  async function hangup() {
+    console.log("hangup");
+    setGettingCall(false);
+    connecting.current = false;
+    streamCleanUp();
+    firebaseCleanUp();
+    if (pc.current) {
+      pc.current.close();
+    }
+  }
+
   // Helper function
 
   async function getStream() {
@@ -133,6 +151,34 @@ export default function CallingScreen() {
       return mediaStream;
     } catch (err) {
       console.log("err", err);
+    }
+  }
+
+  async function streamCleanUp() {
+    console.log("streamCleanUp");
+    if (localStream) {
+      localStream.getTracks().forEach((t) => t.stop());
+      localStream.release();
+    }
+    setLocalStream(null);
+    setRemoteStream(null);
+  }
+
+  async function firebaseCleanUp() {
+    console.log("firebaseCleanUp");
+    const cRef = doc(db, "meet", userB_uid);
+    if (cRef) {
+      const qee = query(collection(cRef, "callee"));
+      const calleeCandidate = await getDocs(qee);
+      calleeCandidate.forEach(async (candidate) => {
+        await deleteDoc(candidate.ref);
+      });
+      const qer = query(collection(cRef, "caller"));
+      const callerCandidate = await getDocs(qer);
+      callerCandidate.forEach(async (candidate) => {
+        await deleteDoc(candidate.ref);
+      });
+      deleteDoc(cRef);
     }
   }
 
@@ -159,6 +205,19 @@ export default function CallingScreen() {
         }
       });
     });
+  }
+
+  // Displays local stream on calling
+  // Displays both local and remote stream once call is connected
+  if (localStream) {
+    console.log("localStream");
+    return (
+      <VideoScreen
+        hangup={hangup}
+        localStream={localStream}
+        remoteStream={remoteStream}
+      ></VideoScreen>
+    );
   }
 
   return (
