8878a22f509f88e90899f747c971c31d371f96ba
chinfanster <chinfanster@gmail.com>
2023-04-26 14:57:32 -0700

    clean code

--- app/screens/ProfileInfoListScreen.js
+++ app/screens/ProfileInfoListScreen.js
@@ -64,7 +64,6 @@ function ProfileInfoListScreen({ create }) {
 
   const route = useRoute();
   const userB = route.params.ProfileUser;
-  console.log("userB", userB);
 
   const upToDateUserBData = useGetSingleCustomerInfo(userB.uid);
 
