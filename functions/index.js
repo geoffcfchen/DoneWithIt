d0a4196fa03883f47f06193c4a7a7697c9453576
chinfanster <chinfanster@gmail.com>
2023-05-05 14:25:47 -0700

    deploy createPaymentIntent firebase function

--- functions/index.js
+++ functions/index.js
@@ -7,3 +7,28 @@ const functions = require("firebase-functions");
 //   functions.logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+
+const admin = require("firebase-admin");
+const stripe = require("stripe")(functions.config().stripe.secret);
+
+admin.initializeApp();
+
+exports.createPaymentIntent = functions.https.onCall(async (data, context) => {
+  const priceId = data.priceId;
+
+  try {
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: 1000, // replace with the desired amount
+      currency: "usd",
+      metadata: { integration_check: "accept_a_payment" },
+    });
+
+    return { clientSecret: paymentIntent.client_secret };
+  } catch (error) {
+    console.error("Error creating payment intent:", error);
+    throw new functions.https.HttpsError(
+      "internal",
+      "Error creating payment intent"
+    );
+  }
+});
